6b9afb02d3baf9517a9129251d6b17c118dc59f2
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-11-16 12:16:14 +0530

    js updated pwa

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,23 @@ const FILES = [
   "/main.js",
   "/createsection.js",
   "/pwa.js",
+  "/ai/browserAI.js",
+  "/ai/imageEnhancement.js",
   "/JS/homesection.js",
-
   "/logo.png",
-  "/icons/icon-72x72.png",
+ "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-256x256.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-48x48.png",
+  "/icons/icon-512x512.png",
+  "/icons/Astudio logo.png"
+
+
 
   // LOCAL FONTS
   "/fonts/Veturi.ttf",
